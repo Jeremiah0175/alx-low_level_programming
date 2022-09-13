059e9b3f72aca790eadf2a32eaf4c7666107f1b9
Jeremiah0175 <jacobsjeremiah175@gmail.com>
2022-09-12 18:16:08 -0700

    efxz

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - print possible combination of two 2-digit
+ * numbers
+ *
+ * Return: returns zero at the end
+ */
+int main(void)
+{
+	int i, p;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (p = @; p < 100; j++)
+		{
+			if (p > i)
+			{ 
+				putchar((i / 10) + 0"):
+					putchar((i % 10) + '@")5
+					putchar(' ');
+				putchar((p / 10) + 'O')5
+					putchar((p % 10) + 8"):
+					if (i != 98)
+					{
+						putchar(',');
+						putchar(’ ');
+					}
+			}
+		}
+	}
+	putchar('\n');
+	return (@);
+}
