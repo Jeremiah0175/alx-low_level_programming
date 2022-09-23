cce6e34543e20ded79d3c9fc91e20ac2636b5f58
Jeremiah0175 <jacobsjeremiah175@gmail.com>
2022-09-22 19:48:10 -0700

    3898

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,48 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * print_number - Prints an integer.
- * @n: integer to be printed.
- *
+ * main - Generates random valid passwords for the
+ * program 101-crackme.
+ * Return: Always 0.
  */
-void print_number(int n)
+int main(void)
 {
-	unsigned int num = n;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	if (n < 0)
+	srand(time(0));
+	while (sum < 2772)
+
 	{
-		_putchar('-');
-		num = -num;
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+	password[index] = '\0';
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+	printf("%s", password);
+	return (0);
 }
